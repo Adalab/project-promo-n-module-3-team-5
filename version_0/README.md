ee0c9a3aa8174dc6ea3ef486e7736028b3b34caa
J. Natalia Ramirez <jnataliaramirez@gmail.com>
2021-09-17 15:58:41 +0200

    Add html, project previous and add folder project previous

---
+++ version_0/README.md
@@ -0,0 +1,25 @@
+# PROYECTO MÓDULO 2 EQUIPO 4
+
+# Awesome Profile-Cards
+
+Awesome Profile-Cards es una aplicación web que nos permite crear una tarjeta de visita personalizada de forma instantánea.
+
+La web está formada por una página landing que enlaza con otra en la que personalizar contenido y diseño y que está diseñada para ser compatida.
+
+Para poder realizar tu tarjeta virtual debes elegir una paleta de colores, rellenar los campos con tus datos y pulsar el botón de generar. El resultado es un link con la URL de tu tarjeta virtual, que podrás compartir en twiter o cualquier otro medio.
+
+### Arrancar el proyecto:
+
+Una vez hemos instalado las dependencias **el proyecto hay que arrancarlo cada vez.** Para ello ejecuta el comando:
+
+```bash
+npm start
+```
+
+Este comando:
+
+- **Abre una ventana de Chrome y muestra la página web**.
+
+### el proyecto también está publicado en GitHubPages:
+
+El enlace se encuentra en la página principal del repositorio, en la parte superior, al lado de la descripción.
---
+++ version_0/src/js/02design.js
@@ -0,0 +1,40 @@
+const coloursDesign = document.querySelectorAll('.js_design');
+const paintColor = document.querySelectorAll('.js_designColor');
+
+function paintPreview(ev) {
+  const paletteClicked = parseInt(ev.target.id);
+  if (paletteClicked === 1) {
+    paintDefault();
+  } else if (paletteClicked === 2) {
+    paintRed();
+  } else {
+    paintYellow();
+  }
+}
+function paintRed() {
+  for (const eachSection of paintColor) {
+    eachSection.classList.remove('defaultpalette');
+    eachSection.classList.remove('yellowpalette');
+    eachSection.classList.add('redpalette');
+  }
+}
+
+function paintYellow() {
+  for (const eachSection of paintColor) {
+    eachSection.classList.remove('defaultpalette');
+    eachSection.classList.remove('redpalette');
+    eachSection.classList.add('yellowpalette');
+  }
+}
+
+function paintDefault() {
+  for (const eachSection of paintColor) {
+    eachSection.classList.remove('yellowpalette');
+    eachSection.classList.remove('redpalette');
+    eachSection.classList.add('defaultpalette');
+  }
+}
+
+for (const eachinput of coloursDesign) {
+  eachinput.addEventListener('click', paintPreview);
+}
