a40e2cf1bf9422be593e31bb7a9920385007ca48
Natalia Ramirez <jnataliaramirez@gmail.com>
2021-09-27 13:38:54 +0200

    Do partials Preview without styles

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,17 @@
 // Fichero src/components/App.js
 // Migración imagenes
 import logoAdalab from "../images/logo-adalab.png";
-import imagePreview from "../images/previewImg.jpg";
+
 // useState
 import { useState } from "react";
+
+
 // Componentes
 import Header from './Header';
+import Preview from './Preview';
 // Estilos
 import '../styles/App.scss';
-import '../styles/core/Reset.scss';
+import '../styles/core/Reset.scss'
 
 function App() {
   // Variables colapsables
@@ -76,17 +79,8 @@ function App() {
     } 
   };
 
-  // Mostrar Nombre y Trabajo en la tarjeta predefinida
-  let nameToDisplay = showToDisplay(data.name,'Nombre Apellidos');
-  let jobToDisplay = showToDisplay(data.job,'Front-end Developer');
+ 
 
-  function showToDisplay (dataSome, result) {
-    if ( dataSome === '') {
-      return result
-    } else {
-      return dataSome
-    }
-  }
 
   // Escuchadores de apertura y cierre de secciones 
   const handleCollapsableDesign = (ev) => {
@@ -127,61 +121,7 @@ function App() {
       <div className="page-wrapper">
        <Header />
         <main className="wholeMain">
-          <section className="preview">
-            <button className="preview__button js_resetButton">
-              <i className="fas fa-trash-alt"></i>reset
-            </button>
-            <article className="preview__card">
-              {/* ! empiezan estilos de la card */}
-              <span className="preview__pretty  js_designColor"></span> 
-              <h2 className="preview__name js_previewName js_design">
-                {nameToDisplay}
-              </h2>
-              <h3 className="preview__description js_previewDescription">
-                {jobToDisplay}
-              </h3>
-              <div
-                style={{
-                  backgroundImage: `url(${imagePreview})`,
-                }}
-                className="preview__image js__profile-image"
-              ></div>
-              <nav className="preview__icons">
-                <a
-                  className="preview__icons--measures"
-                  href={"+34" + data.phone}
-                  title="phone"
-                  name="phone"
-                >
-                  <i className="fas fa-mobile-alt"></i>
-                </a>
-                <a
-                  className="preview__icons--measures"
-                  href={`mailto:${data.email}`}
-                  title="message"
-                >
-                  <i className="far fa-envelope"></i>
-                </a>
-                <a
-                  className="preview__icons--measures js_designColor js_previewLinkedin"
-                  href={`https://linkedin.com/in/${data.linkedin}`}
-                  target="_blank"
-                  title="linkedin"
-                >
-                  <i className="fab fa-linkedin-in"></i>
-                </a>
-                <a
-                  className="preview__icons--measures js_designColor js_previewGithub"
-                  href={`https://github.com/${data.github}`}
-                  target="_blank"
-                  title="github"
-                >
-                  <i className="fab fa-github-alt"></i>
-                </a>
-              </nav>
-            </article>
-          </section>
-
+         <Preview data={data}/>
           <form onSubmit={handleForm} className="form">
             <div>
               <div
