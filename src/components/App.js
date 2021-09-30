2c7486b69bea55bab3eedfb2fb9b53b289c79c60
Clara Vilela <claravilelajato@gmail.com>
2021-09-30 08:28:48 +0200

    changes on newbranch

--- src/components/App.js
+++ src/components/App.js
@@ -1,41 +1,41 @@
 //Ficheros componentes
-import Header from "./Header";
-import Design from "./Design";
+import Header from './Header';
+import Design from './Design';
 // import Preview from "./Preview";
-import Share from "./Share";
-import Footer from "./Footer";
+import Share from './Share';
+import Footer from './Footer';
 
 // Estilos
-import "../styles/App.scss";
-import "../styles/core/Reset.scss";
-import "../styles/components/Preview.scss";
+import '../styles/App.scss';
+import '../styles/core/Reset.scss';
+import '../styles/components/Preview.scss';
 
 // Migración imagenes
-import logoAwesone from "../images/logo-awesome-profile-cards.svg";
-import imagePreview from "../images/previewImg.jpg";
+import logoAwesone from '../images/logo-awesome-profile-cards.svg';
+import imagePreview from '../images/previewImg.jpg';
 
 // Otros
-import { useState } from "react";
+import { useState } from 'react';
 
 function App() {
   // Variables colapsables
-  const [collapseDesign, setCollapseDesign] = useState("");
-  const [collapseFill, setCollapseFill] = useState("hidden");
-  const [collapseShare, setCollapseShare] = useState("hidden");
+  const [collapseDesign, setCollapseDesign] = useState('');
+  const [collapseFill, setCollapseFill] = useState('hidden');
+  const [collapseShare, setCollapseShare] = useState('hidden');
 
   // Variable de las paletas (radio)
-  const [paletteColor, setPaletteColor] = useState("");
+  const [paletteColor, setPaletteColor] = useState('');
 
   // Variable del Input del formulario (array de datos)
   const [data, setData] = useState({
     palette: 1,
-    name: "",
-    job: "",
-    phone: "",
-    email: "",
-    linkedin: "",
-    github: "",
-    photo: "",
+    name: '',
+    job: '',
+    phone: '',
+    email: '',
+    linkedin: '',
+    github: '',
+    photo: '',
   });
 
   //función para previsualizar imagen usuaria:
@@ -50,7 +50,7 @@ function App() {
    */
   function getImage(e) {
     const myFile = e.currentTarget.files[0];
-    fr.addEventListener("load", writeImage);
+    fr.addEventListener('load', writeImage);
     fr.readAsDataURL(myFile);
   }
 
@@ -72,7 +72,7 @@ function App() {
 
   function getPhotoUrl(data, placeholder) {
     if (placeholder) {
-      return data.photo === "" ? imagePreview : data.photo;
+      return data.photo === '' ? imagePreview : data.photo;
     } else {
       return data.photo;
     }
@@ -85,24 +85,24 @@ function App() {
   // no de puede utilizar en handleInput (que llama names)
   const handlePalette = (ev) => {
     const whichRadio = ev.target.id;
-    if (whichRadio === "palette-1") {
+    if (whichRadio === 'palette-1') {
       setData({
         ...data,
         palette: parseInt(ev.target.value),
       });
-      setPaletteColor("defaultpalette");
-    } else if (whichRadio === "palette-2") {
+      setPaletteColor('defaultpalette');
+    } else if (whichRadio === 'palette-2') {
       setData({
         ...data,
         palette: parseInt(ev.target.value),
       });
-      setPaletteColor("redpalette");
-    } else if (whichRadio === "palette-3") {
+      setPaletteColor('redpalette');
+    } else if (whichRadio === 'palette-3') {
       setData({
         ...data,
         palette: parseInt(ev.target.value),
       });
-      setPaletteColor("yellowpalette");
+      setPaletteColor('yellowpalette');
     }
   };
 
@@ -110,30 +110,30 @@ function App() {
   // array Data
   const handleInput = (ev) => {
     const wichInput = ev.currentTarget.name;
-    if (wichInput === "name") {
+    if (wichInput === 'name') {
       setData({
         ...data,
         name: ev.currentTarget.value,
       });
-    } else if (wichInput === "job") {
+    } else if (wichInput === 'job') {
       setData({ ...data, job: ev.currentTarget.value });
-    } else if (wichInput === "phone") {
+    } else if (wichInput === 'phone') {
       setData({ ...data, phone: ev.currentTarget.value });
-    } else if (wichInput === "email") {
+    } else if (wichInput === 'email') {
       setData({ ...data, email: ev.currentTarget.value });
-    } else if (wichInput === "linkedin") {
+    } else if (wichInput === 'linkedin') {
       setData({ ...data, linkedin: ev.currentTarget.value });
-    } else if (wichInput === "github") {
+    } else if (wichInput === 'github') {
       setData({ ...data, github: ev.currentTarget.value });
     }
   };
 
   // Mostrar Nombre y Trabajo en la tarjeta predefinida
-  let nameToDisplay = showToDisplay(data.name, "Nombre Apellidos");
-  let jobToDisplay = showToDisplay(data.job, "Front-end Developer");
+  let nameToDisplay = showToDisplay(data.name, 'Nombre Apellidos');
+  let jobToDisplay = showToDisplay(data.job, 'Front-end Developer');
 
   function showToDisplay(dataSome, result) {
-    if (dataSome === "") {
+    if (dataSome === '') {
       return result;
     } else {
       return dataSome;
@@ -142,49 +142,48 @@ function App() {
 
   // Escuchadores de apertura y cierre de secciones
   const handleCollapsableDesign = (ev) => {
-    if (collapseDesign === "hidden") {
-      setCollapseDesign("");
-      setCollapseFill("hidden");
-      setCollapseShare("hidden");
+    if (collapseDesign === 'hidden') {
+      setCollapseDesign('');
+      setCollapseFill('hidden');
+      setCollapseShare('hidden');
     } else {
-      setCollapseDesign("hidden");
+      setCollapseDesign('hidden');
       console.log(collapseDesign);
     }
   };
   const handleCollapsableFill = (ev) => {
-    if (collapseFill === "hidden") {
-      setCollapseFill("");
-      setCollapseDesign("hidden");
-      setCollapseShare("hidden");
+    if (collapseFill === 'hidden') {
+      setCollapseFill('');
+      setCollapseDesign('hidden');
+      setCollapseShare('hidden');
     } else {
-      setCollapseFill("hidden");
+      setCollapseFill('hidden');
       console.log(collapseDesign);
     }
   };
   const handleCollapsableShare = (ev) => {
-    if (collapseShare === "hidden") {
-      setCollapseShare("");
-      setCollapseDesign("hidden");
-      setCollapseFill("hidden");
+    if (collapseShare === 'hidden') {
+      setCollapseShare('');
+      setCollapseDesign('hidden');
+      setCollapseFill('hidden');
     } else {
-      setCollapseShare("hidden");
+      setCollapseShare('hidden');
       console.log(collapseDesign);
     }
   };
 
-
   const handleForm = () => {};
 
   return (
-    <div className="root">
-      <div className="page-wrapper">
+    <div className='root'>
+      <div className='page-wrapper'>
         <Header />
-        <main className="wholeMain">
-          <section className="preview">
-            <button className="preview__button js_resetButton">
-              <i className="fas fa-trash-alt"></i>reset
+        <main className='wholeMain'>
+          <section className='preview'>
+            <button className='preview__button js_resetButton'>
+              <i className='fas fa-trash-alt'></i>reset
             </button>
-            <article className="preview__card">
+            <article className='preview__card'>
               <span className={`preview__pretty ${paletteColor}`}></span>
               <h2 className={`preview__name ${paletteColor}`}>
                 {nameToDisplay}
@@ -194,45 +193,45 @@ function App() {
                 style={{
                   backgroundImage: `url(${getPhotoUrl(data, true)})`,
                 }}
-                className="preview__image js__profile-image"
+                className='preview__image js__profile-image'
               ></div>
-              <nav className="preview__icons">
+              <nav className='preview__icons'>
                 <a
                   className={`preview__icons--measures  ${paletteColor}`}
-                  href={"+34" + data.phone}
-                  title="phone"
-                  name="phone"
+                  href={'+34' + data.phone}
+                  title='phone'
+                  name='phone'
                 >
-                  <i className="fas fa-mobile-alt"></i>
+                  <i className='fas fa-mobile-alt'></i>
                 </a>
                 <a
                   className={`preview__icons--measures ${paletteColor}`}
                   href={`mailto:${data.email}`}
-                  title="message"
+                  title='message'
                 >
-                  <i className="far fa-envelope"></i>
+                  <i className='far fa-envelope'></i>
                 </a>
                 <a
                   className={`preview__icons--measures ${paletteColor}`}
                   href={`https://linkedin.com/in/${data.linkedin}`}
-                  target="_blank"
-                  title="linkedin"
+                  target='_blank'
+                  title='linkedin'
                 >
-                  <i className="fab fa-linkedin-in"></i>
+                  <i className='fab fa-linkedin-in'></i>
                 </a>
                 <a
                   className={`preview__icons--measures  ${paletteColor}`}
                   href={`https://github.com/${data.github}`}
-                  target="_blank"
-                  title="github"
+                  target='_blank'
+                  title='github'
                 >
-                  <i className="fab fa-github-alt"></i>
+                  <i className='fab fa-github-alt'></i>
                 </a>
               </nav>
             </article>
           </section>
 
-          <form onSubmit={handleForm} className="form">
+          <form onSubmit={handleForm} className='form'>
             <Design
               collapseDesign={collapseDesign}
               handleCollapse={handleCollapsableDesign}
@@ -240,137 +239,137 @@ function App() {
             />
 
             <div>
-              <div className="topBorder"></div>
+              <div className='topBorder'></div>
               <div
-                className="collapsable js_collapsable_fill "
+                className='collapsable js_collapsable_fill '
                 onClick={handleCollapsableFill}
               >
-                <i className="far fa-keyboard collapsable__iconStart"></i>
-                <h3 className="collapsable__title">Rellena</h3>
+                <i className='far fa-keyboard collapsable__iconStart'></i>
+                <h3 className='collapsable__title'>Rellena</h3>
                 <i
                   className={
-                    "fas fa-chevron-" +
-                    (collapseFill === "hidden" ? "down" : "up") +
-                    " collapsable__iconEnd js_iconEnd"
+                    'fas fa-chevron-' +
+                    (collapseFill === 'hidden' ? 'down' : 'up') +
+                    ' collapsable__iconEnd js_iconEnd'
                   }
                 ></i>
               </div>
-              <section className={collapseFill + " fill"} id="fill">
-                <fieldset className="fill__first">
-                  <div className="fill__first--name">
-                    <label htmlFor="fullName" className="fill__first--name-lab">
+              <section className={collapseFill + ' fill'} id='fill'>
+                <fieldset className='fill__first'>
+                  <div className='fill__first--name'>
+                    <label htmlFor='fullName' className='fill__first--name-lab'>
                       Nombre completo *
                     </label>
                     <input
                       value={data.name}
-                      className="fill__first--name-inp js_allInputs"
-                      placeholder="Ej: Sally Jill"
-                      id="name"
-                      type="text"
-                      name="name"
+                      className='fill__first--name-inp js_allInputs'
+                      placeholder='Ej: Sally Jill'
+                      id='name'
+                      type='text'
+                      name='name'
                       onChange={handleInput}
                     />
                   </div>
-                  <div className="fill__first--position">
+                  <div className='fill__first--position'>
                     <label
-                      htmlFor="position"
-                      className="fill__first--position-lab"
+                      htmlFor='position'
+                      className='fill__first--position-lab'
                     >
                       Puesto *
                     </label>
                     <input
                       value={data.job}
-                      className="fill__first--position-inp js_allInputs"
-                      placeholder="Ej: Front-end unicorn"
-                      id="job"
-                      type="text"
-                      name="job"
+                      className='fill__first--position-inp js_allInputs'
+                      placeholder='Ej: Front-end unicorn'
+                      id='job'
+                      type='text'
+                      name='job'
                       onChange={handleInput}
                     />
                   </div>
                 </fieldset>
-                <fieldset className="fill__second">
-                  <label className="fill__second-label">
+                <fieldset className='fill__second'>
+                  <label className='fill__second-label'>
                     Imagen de perfil *
                   </label>
-                  <div className="fill__second-buttoncheck">
-                    <label className="fill__second-button" htmlFor="photo">
+                  <div className='fill__second-buttoncheck'>
+                    <label className='fill__second-button' htmlFor='photo'>
                       Añadir imagen
                     </label>
                     <input
-                      type="file"
+                      type='file'
                       onChange={getImage}
-                      id="photo"
-                      className="hidden_button js__profile-upload-btn"
+                      id='photo'
+                      className='hidden_button js__profile-upload-btn'
                     />
                     <div
-                      className="fill__second-checkbox js__profile-preview"
+                      className='fill__second-checkbox js__profile-preview'
                       style={{
                         backgroundImage: `url(${getPhotoUrl(data, false)})`,
                       }}
                     ></div>
                   </div>
                 </fieldset>
-                <fieldset className="fill__third">
-                  <div className="fill__third--mail">
-                    <label htmlFor="email" className="fill__third--mail-lab">
+                <fieldset className='fill__third'>
+                  <div className='fill__third--mail'>
+                    <label htmlFor='email' className='fill__third--mail-lab'>
                       Email *
                     </label>
                     <input
-                      className="fill__third--mail-inp"
-                      placeholder="Ej: sally-hill@gmail.com"
-                      id="email"
-                      type="email"
-                      name="email"
+                      className='fill__third--mail-inp'
+                      placeholder='Ej: sally-hill@gmail.com'
+                      id='email'
+                      type='email'
+                      name='email'
                       value={data.email}
                       onChange={handleInput}
                     />
                   </div>
-                  <div className="fill__third--tel">
-                    <label htmlFor="telephone" className="fill__third--tel-lab">
+                  <div className='fill__third--tel'>
+                    <label htmlFor='telephone' className='fill__third--tel-lab'>
                       Teléfono
                     </label>
                     <input
-                      className="fill__third--tel-inp js_allInputs"
-                      placeholder="Ej: 555-55-55-55"
-                      id="phone"
-                      type="tel"
-                      name="phone"
+                      className='fill__third--tel-inp js_allInputs'
+                      placeholder='Ej: 555-55-55-55'
+                      id='phone'
+                      type='tel'
+                      name='phone'
                       value={data.phone}
                       onChange={handleInput}
                     />
                   </div>
-                  <div className="fill__third--linked">
+                  <div className='fill__third--linked'>
                     <label
-                      htmlFor="linkedinName"
-                      className="fill__third--linked-lab"
+                      htmlFor='linkedinName'
+                      className='fill__third--linked-lab'
                     >
                       Linkedin *
                     </label>
                     <input
-                      className="fill__third--linked-inp js_allInputs"
-                      placeholder="Ej: linkedin.com/in/sally.hill"
-                      id="linkedin"
-                      type="text"
-                      name="linkedin"
+                      className='fill__third--linked-inp js_allInputs'
+                      placeholder='Ej: linkedin.com/in/sally.hill'
+                      id='linkedin'
+                      type='text'
+                      name='linkedin'
                       value={data.linkedin}
                       onChange={handleInput}
                     />
                   </div>
                   {/*<!-- se consideraría type text para un texto que empieza con arroba¿?-->*/}
-                  <div className="fill__third--git">
+                  <div className='fill__third--git'>
                     <label
-                      htmlFor="gitHubName"
-                      className="fill__third--git-lab"
+                      htmlFor='gitHubName'
+                      className='fill__third--git-lab'
                     >
                       Github *
                     </label>
                     <input
-                      className="fill__third--git-inp js_allInputs"
-                      placeholder="Ej: @sally-hill"
-                      id="github"
-                      type="text"
-                      name="github"
+                      className='fill__third--git-inp js_allInputs'
+                      placeholder='Ej: @sally-hill'
+                      id='github'
+                      type='text'
+                      name='github'
                       value={data.github}
                       onChange={handleInput}
                     />
@@ -379,24 +378,11 @@ function App() {
                 </fieldset>
               </section>
             </div>
-            <div>
-              <div className="topBorder"></div>
-              <div
-                className="collapsable js_collapsable"
-                onClick={handleCollapsableShare}
-              >
-                <i className="fas fa-share-alt collapsable__iconStart"></i>
-                <h3 className="collapsable__title">Comparte</h3>
-                <i
-                  className={
-                    "fas fa-chevron-" +
-                    (collapseShare === "hidden" ? "down" : "up") +
-                    " collapsable__iconEnd js_iconEnd"
-                  }
-                ></i>
-              </div>
-            </div>
-            <Share />
+
+            <Share
+              handleCollapsableShare={handleCollapsableShare}
+              collapseShare={collapseShare}
+            />
           </form>
         </main>
       </div>
--- src/components/Share.js
+++ src/components/Share.js
@@ -1,36 +1,53 @@
 import '../styles/components/Share.scss';
 
-function Share() {
-  const collapseShare = () => {};
-
+function Share(props) {
   return (
-    <section className={collapseShare} id='share'>
-      <fieldset className='share'>
-        <div className='share__second'>
-          <button className='share__button js_shareButton'>
-            <i className='far fa-address-card'></i> Crear tarjeta
-          </button>
-        </div>
-        <div className='share__topBorder'> </div>
-        <div className='share__collapsable js_createdCard'>
-          <h3 className='share__collapsable__title js_notCreate'>
-            La tarjeta ha sido creada
-          </h3>
-          <p className='card__http js_urlCard'>
-            https://awesome-profile-card.com?id=A456DF0001
-          </p>
-          <button className='share__collapsable__button js_twiterBtn'>
-            <a
-              className='share__collapsable__button--link'
-              href=''
-              target='_blank'
-            >
-              <i className='icon3 fab fa-twitter'></i>&nbsp;Compartir en twitter
-            </a>
-          </button>
-        </div>
-      </fieldset>
-    </section>
+    <div>
+      <div className='topBorder'></div>
+      <div
+        className='collapsable js_collapsable'
+        onClick={props.handleCollapsableShare}
+      >
+        <i className='fas fa-share-alt collapsable__iconStart'></i>
+        <h3 className='collapsable__title'>Comparte</h3>
+        <i
+          className={
+            'fas fa-chevron-' +
+            (props.collapseShare === 'hidden' ? 'down' : 'up') +
+            ' collapsable__iconEnd js_iconEnd'
+          }
+        ></i>
+      </div>
+
+      <section className={props.collapseShare} id='share'>
+        <fieldset className='share'>
+          <div className='share__second'>
+            <button className='share__button js_shareButton'>
+              <i className='far fa-address-card'></i> Crear tarjeta
+            </button>
+          </div>
+          <div className='share__topBorder'> </div>
+          <div className='share__collapsable js_createdCard'>
+            <h3 className='share__collapsable__title js_notCreate'>
+              La tarjeta ha sido creada
+            </h3>
+            <p className='card__http js_urlCard'>
+              https://awesome-profile-card.com?id=A456DF0001
+            </p>
+            <button className='share__collapsable__button js_twiterBtn'>
+              <a
+                className='share__collapsable__button--link'
+                href='./'
+                target='_blank'
+              >
+                <i className='icon3 fab fa-twitter'></i>&nbsp;Compartir en
+                twitter
+              </a>
+            </button>
+          </div>
+        </fieldset>
+      </section>
+    </div>
   );
 }
 
