5ff77241afebaa18b1521d33efc01d9e6f2d5b52
Natalia Ramirez <jnataliaramirez@gmail.com>
2021-10-11 12:15:07 +0200

    Create folder web and files server

--- src/index.js
+++ src/index.js
@@ -1,6 +1,26 @@
 // Fichero src/index.js
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+// Importamos los dos módulos de NPM necesarios para trabajar
+const express = require("express");
+const cors = require("cors");
+
+// Creamos el servidor
+const server = express();
+
+// Configuramos el servidor
+server.use(cors());
+server.use(express.json());
+
+// Arrancamos el servidor en el puerto 3000
+const serverPort = 3000;
+server.listen(serverPort, () => {
+   console.log(`Server listening at http://localhost:${serverPort}`);
+});
+
+// Escribimos los endpoints que queramos
+server.get("/users", (req, res) => {
+   const response = {
+      users: [{ name: "Sofía" }, { name: "María" }],
+   };
+   res.json(response);
+});
