c49e7fcc767a39e1bb82b90d0cc0da0da067d020
Daniela <danisalazarcueto7@gmail.com>
2021-10-18 13:44:02 +0200

    modified database and add new endpoint with select

--- src/index.js
+++ src/index.js
@@ -1,17 +1,17 @@
 // Importamos los dos módulos de NPM necesarios para trabajar
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
 
 // Creamos el servidor
 const server = express();
 
 // Configuramos el servidor
 server.use(cors());
-server.use(express.json({ limit: '50mb' }));
+server.use(express.json({ limit: "50mb" }));
 
 // set template engine middlewares
-server.set('view engine', 'ejs');
-const Database = require('better-sqlite3');
+server.set("view engine", "ejs");
+const Database = require("better-sqlite3");
 
 // Arrancamos el servidor en el puerto 4001
 const serverPort = 4001;
@@ -20,51 +20,65 @@ server.listen(serverPort, () => {
 });
 
 // init and config data base
-const db = new Database('./src/database.db', {
+const db = new Database("./src/database.db", {
   // this line log in console all data base queries
   verbose: console.log,
 });
 //static server
-const serverStaticPath = './public';
+const serverStaticPath = "./public";
 server.use(express.static(serverStaticPath));
 
 // template engines
-server.get('/interactive_card/:id', (req, res) => {
+server.get("/interactive_card/:id", (req, res) => {
   console.log("Paso por aqui");
-  const query = db.prepare(`SELECT * FROM card_interactive WHERE id =?`)
+  const query = db.prepare(`SELECT * FROM card_interactive WHERE id =?`);
   const data = query.get(req.params.id);
 
-  res.render('interactive_card', data);
-})
+  res.render("interactive_card", data);
+});
 
 // Escribimos los endpoints que queramos
-server.post('/card', (req, res) => {
+server.post("/card", (req, res) => {
   let response = {};
   console.log(req.body.name);
-  if (req.body.name === '' || req.body.name === undefined) {
-    const error = 'debe indicar su nombre';
+  if (req.body.name === "" || req.body.name === undefined) {
+    const error = "debe indicar su nombre";
     response = { success: false, error: error };
-  } else if (req.body.job === '') {
-    const error = 'indica tu trabajo';
+  } else if (req.body.job === "") {
+    const error = "indica tu trabajo";
     response = { success: false, error: error };
-  } else if (req.body.photo === '') {
-    const error = 'pon tu foto';
+  } else if (req.body.photo === "") {
+    const error = "pon tu foto";
     response = { success: false, error: error };
-  } else if (req.body.email === '') {
-    const error = 'indica tu email';
+  } else if (req.body.email === "") {
+    const error = "indica tu email";
     response = { success: false, error: error };
-  } else if (req.body.phone === '') {
-    const error = 'indica tu teléfono';
+  } else if (req.body.phone === "") {
+    const error = "indica tu teléfono";
     response = { success: false, error: error };
-  } else if (req.body.linkedin === '') {
-    const error = 'indica tu linkedin';
+  } else if (req.body.linkedin === "") {
+    const error = "indica tu linkedin";
     response = { success: false, error: error };
-  } else if (req.body.github === '') {
-    const error = 'indica tu github';
+  } else if (req.body.github === "") {
+    const error = "indica tu github";
     response = { success: false, error: error };
   } else {
-    const cardURL = 'este es el enlace';
-    response = { success: true, cardURL: cardURL };
+    const query = db.prepare(
+      "INSERT INTO card_interactive(palette, name, job, phone, photo, github, linkedin) VALUES (?, ?, ?, ?, ?, ?, ?)"
+    );
+    const result = query.run(
+      req.body.palette,
+      req.body.name,
+      req.body.job,
+      req.body.phone,
+      req.body.photo,
+      req.body.github,
+      req.body.linkedin
+    );
+    response.success = true;
+    response.cardURL = `http://localhost:4001/interactive_card/${result.lastInsertRowid}`;
+    //const cardURL = 'este es el enlace';
+    //response = { success: true, cardURL: cardURL };
   }
 
   res.json(response);
